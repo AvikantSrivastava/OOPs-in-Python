b8f9f505a11715941430de463489d68e03d3d3b5
Avikant Srivastava <asdavikant@gmail.com>
2020-05-02 22:16:41 +0530

    day3

---
+++ Day3/Challenge1  Implement a Banking Account.py
@@ -0,0 +1,10 @@
+class Account:
+    def __init__(self, title=None, balance=0):
+        self.title = title
+        self.balance = balance
+
+
+class SavingsAccount(Account):
+    def __init__(self, title=None, balance=0, interestRate=0):
+        super().__init__(title, balance)
+        self.interestRate = interestRate
